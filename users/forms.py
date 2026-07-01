692001bf1b8e381b31481fe2a0c415c95f3e2ad1
madayuki <abed.kurniawan76@gmail.com>
2026-07-01 23:33:44 +0800

    push

--- users/forms.py
+++ users/forms.py
@@ -1,28 +1,48 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Row, Column, Layout
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(
-        required=True,
-        help_text= "150 characters or fewer. Letters, digits and @/./+/-/_ only."
-    )
-    first_name = forms.TextInput(
-        required=True,
-        help_text= "150 characters or fewer. Letters, digits and @/./+/-/_ only."
-    )
-    last_name = forms.TextInput(
-        required=True,
-        help_text= "150 characters or fewer. Letters, digits and @/./+/-/_ only."
-    )
+    # email = forms.EmailField(
+    #     required=True,
+    #     help_text= "150 characters or fewer. Letters, digits and @/./+/-/_ only."
+    # )
+    # first_name = forms.CharField(
+    #     help_text="150 characters or fewer. Letters, digits and @/./+/-/_ only.", 
+    #     max_length=150, 
+    #     required=False
+    # )
+    # last_name = forms.CharField(
+    #     help_text="150 characters or fewer. Letters, digits and @/./+/-/_ only.", 
+    #     max_length=150, 
+    #     required=False
+    # )
 
     class Meta:
         model = User
         fields = [
             'username',
-            'first_name',
-            'last_name',
-            'email',
+            # 'first_name',
+            # 'last_name',
+            # 'email',
             'password1',
-            'password2'
+            # 'password2'
         ]
+    
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        
+        self.helper.layout = Layout(
+            'username',
+            # Row(
+            #     Column('first_name'),
+            #     Column('last_name')
+            # ),
+            # 'email',
+            'password1',
+            # 'password2'
+        )
